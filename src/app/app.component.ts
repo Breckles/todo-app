758b2c104b61e21dac53b8740fee68eb708255a2
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-12-01 11:46:12 -0500

    changed the theme switcher implementation to use css variables instead of scss theme files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Themes } from './shared/themes.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'todoApp';
+export class AppComponent implements OnInit {
+  public currentTheme = Themes.darkTheme;
+
+  ngOnInit(): void {}
+
+  toggleTheme(theme: Themes) {
+    this.currentTheme = theme;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+import { Themes } from '../shared/themes.enum';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output()
+  themeChanged = new EventEmitter<Themes>();
+  @ViewChild('themeToggleButton', { static: true })
+  private themeToggleButtonRef!: ElementRef;
+  private themeToggleButtonEl!: HTMLButtonElement;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.themeToggleButtonEl = this.themeToggleButtonRef
+      .nativeElement as HTMLButtonElement;
   }
 
+  toggleTheme() {
+    if (this.themeToggleButtonEl.classList.contains(Themes.darkTheme)) {
+      this.themeToggleButtonEl.classList.remove(Themes.darkTheme);
+      this.themeToggleButtonEl.classList.add(Themes.lightTheme);
+      this.themeChanged.emit(Themes.lightTheme);
+    } else {
+      this.themeToggleButtonEl.classList.remove(Themes.lightTheme);
+      this.themeToggleButtonEl.classList.add(Themes.darkTheme);
+      this.themeChanged.emit(Themes.darkTheme);
+    }
+  }
 }
